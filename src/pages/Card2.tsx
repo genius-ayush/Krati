ccab966593c6b6d30fe330f90a719157035e8362
ayushsanjayrawal@gmail.com <ayushsanjayrawa@gmail.com>
2024-04-28 10:21:10 +0530

    sixty commit

--- src/pages/Card2.tsx
+++ src/pages/Card2.tsx
@@ -6,8 +6,8 @@ interface Props {
   content : string ; 
 }
 
-const Card2: React.FC<Props> = ({ imagePath , heading , content}) => {
-  
+const Card2 = ({ imagePath , heading , content}) => {
+  const routePath = `/pages/${heading}`;
   return (
     <div className="relative flex flex-col mt-6 text-gray-700 bg-gray-200 shadow-md bg-clip-border rounded-xl h-[500px] pt-9">
       <div
@@ -28,7 +28,7 @@ const Card2: React.FC<Props> = ({ imagePath , heading , content}) => {
         <button
           className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
           type="button">
-            <Link href={heading}>
+            <Link href={routePath}>
           Read More
             </Link>
         </button>
