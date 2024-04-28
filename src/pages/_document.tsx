c3205515505cc2c9e525e13dba1386861137abbe
ayushsanjayrawal@gmail.com <ayushsanjayrawa@gmail.com>
2024-04-28 09:40:36 +0530

    fifth commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,6 @@
 
 import { Html, Head, Main, NextScript } from "next/document";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Navbar2 from "./Navbar2";
+
 export default function Document() {
   return (
     <Html lang="en">
